cfccb2cc31126031a6c965f141e41eed22d3808a
Spitzy <spitzy.dev@gmail.com>
2020-11-12 13:22:02 -0500

    created header/footer and function for custom notifications

--- Assets/JavaScript/script.js
+++ Assets/JavaScript/script.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
 function getCityDetails(cityName) {
 
     if(cityName === "") {
-        alert("please enter city first");
+        hideAway();
     } else {
         var queryUrl = "http://open.mapquestapi.com/geocoding/v1/address?key=wraegWcAhDtVMxIGqitPmixrOzkRkRoA&location=" + cityName;
 
@@ -160,7 +160,7 @@ function initAutocomplete() {
     // Create search box
     const input = document.getElementById("auto-input");
     const searchBox = new google.maps.places.SearchBox(input);
-    map.controls[google.maps.ControlPosition.LEFT].push(input);
+    //map.controls[google.maps.ControlPosition.LEFT].push(input);
     // Take searchbox results and pass it to map.
     map.addListener("bounds_changed", () => {
       searchBox.setBounds(map.getBounds());
@@ -193,3 +193,13 @@ function initAutocomplete() {
       map.fitBounds(bounds);
     });
   }
+
+
+
+function hideAway(){
+    var hide = document.getElementById('hideaway');
+    document.getElementById('hideaway').style.display='block';
+    hide.addEventListener('click', function(){
+        hide.style.display="none";
+    });
+}
